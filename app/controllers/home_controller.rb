bbbdfa399955d5343cee818a5fc4954e50d8dd71
Stephanyan <rabena.stephane@gmail.com>
2018-08-09 17:36:29 +0200

    Add StartScrap instance in index action to fill the database at least once

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -1,5 +1,6 @@
 class HomeController < ApplicationController
   def index
+    StartScrap.new.perform
     @cryptocurrencies = Cryptocurrency.all
     unless params[:cryptocurrency_id].nil?
       @currency = Cryptocurrency.find(params[:cryptocurrency_id])
